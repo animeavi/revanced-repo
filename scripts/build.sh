c12f1981879266d4c0510e49543667451c1f2edc
Zero <zero@cock.li>
2023-12-19 21:52:25 -0500

    dont exclude patches

--- scripts/build.sh
+++ scripts/build.sh
@@ -55,7 +55,7 @@ if [ "$revanced" = 'yes' ]; then
 
     if [ -f "youtube.apk" ]; then
         java -jar revanced-cli.jar patch -m revanced-integrations.apk \
-            -b revanced-patches.jar --exclude "Premium heading" \
+            -b revanced-patches.jar \
             -o build/revanced-nonroot.apk youtube.apk
         echo "YouTube ReVanced build finished"
     else
